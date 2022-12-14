03086cbdda53537f5d920cbe5b26dea3fa70abae
Emmanuel <emmanuelcoryl@gmail.com>
2022-12-14 13:38:11 +0300

    9

---
+++ do-while.js
@@ -0,0 +1,14 @@
+//DO WHILE LOOP
+
+let username = "";
+
+ do{
+  username = window.prompt('enter your name')
+}while (username == "" )
+  console.log("hello",username)*/
+
+  //FOR LOOP!!
+
+  for(let i = 1; i <= 10; i +=1) {
+    console.log(i);
+   }
